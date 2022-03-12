30a52158daa891f6ef64f9d061ebbcdb74d6d0c0
Xarber <62101357+Xarber@users.noreply.github.com>
2022-03-12 09:34:50 +0100

    Add files via upload

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -1,7 +1,9 @@
-/*var userLang = navigator.language;
+var userLang = navigator.language;
 if (userLang.indexOf("it") != -1) {
     document.getElementById('commontext-alsoexplore').innerHTML = "Assicurati di esplorare:";
     document.getElementById('commontext-gotop').innerHTML = "Torna all'inizio";
+
+
 } else if (userLang.indexOf("en") != -1) {
     document.getElementById('commontext-alsoexplore').innerHTML = "Be sure to explore:";
     document.getElementById('commontext-gotop').innerHTML
@@ -14,4 +16,4 @@ if (userLang.indexOf("it") != -1) {
     document.getElementById('commontext-CONTENT').innerHTML = "TRADUCCIÓN";
 
 
-}*/
+}
