66eece70147437887beb26705108c66bf34a27e8
Xarber <62101357+Xarber@users.noreply.github.com>
2022-03-19 14:36:02 +0100

    Add files via upload

---
+++ version/2.0/it-it/download/sources/openme/Open Me.bat
@@ -0,0 +1,58 @@
+@echo off
+mode 38,5
+title ADVERTISEMENT
+echo DON'T USE THIS APP IF YOU'RE EPILEPTIC
+echo Press any key to continue.
+pause >nul
+
+start https://youtu.be/dQw4w9WgXcQ?t=42
+:fase
+title YOU
+color 0f
+echo YOU JUST GOT RICKROLLED!
+title JUST
+color f0
+echo YOU JUST GOT RICKROLLED!
+title GOT
+color 1f
+echo YOU JUST GOT RICKROLLED!
+title RICKROLLED!
+color 2f
+echo YOU JUST GOT RICKROLLED!
+title YOU
+color 3f
+echo YOU JUST GOT RICKROLLED!
+title JUST
+color 4f
+echo YOU JUST GOT RICKROLLED!
+title GOT
+color 5f
+echo YOU JUST GOT RICKROLLED!
+title RICKROLLED!
+color 6f
+echo YOU JUST GOT RICKROLLED!
+title YOU
+color 7f
+echo YOU JUST GOT RICKROLLED!
+title JUST
+color 8f
+echo YOU JUST GOT RICKROLLED!
+title GOT
+color 9f
+echo YOU JUST GOT RICKROLLED!
+title RICKROLLED!
+color af
+echo YOU JUST GOT RICKROLLED!
+title YOU
+color bf
+echo YOU JUST GOT RICKROLLED!
+title JUST
+color cf
+echo YOU JUST GOT RICKROLLED!
+title GOT
+color df
+echo YOU JUST GOT RICKROLLED!
+title RICKROLLED!
+color ef
+echo YOU JUST GOT RICKROLLED!
+goto fase
