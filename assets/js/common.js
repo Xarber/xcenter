ac7b57968a25b12b34f0e4b5ed341799550c56f4
Xarber <62101357+Xarber@users.noreply.github.com>
2022-05-07 13:40:05 +0200

    Notifications Upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -149,19 +149,15 @@ var CommonJS = {
             notificationadduploadinput.setAttribute('class', 'hided');
             notificationadduploadinput.setAttribute('accept', '.xnp');
             notificationadduploadlabel.setAttribute('for', 'new-notifications-upload');
+            notificationadduploadlabel.setAttribute('class', 'hided')
+            notificationadduploadlabel.setAttribute('id', 'new-notifications-upload-label');
             notificationadduploadlabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/></svg>';
             notificationsclose.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
             notificationstitle.innerHTML = 'Notifications';
             notificationsbkg.appendChild(notifications);
             notifications.appendChild(notificationsclose);
-            if (document.getElementById('new-notifications-upload') == null && uploadnotification) {
-                console.log(
-                    '%cNotification Component Missing: Upload Button, adding...',
-                    'background-color: red;padding: 10px;border-radius: 15px;'
-                )
-                notifications.appendChild(notificationadduploadlabel);
-                notifications.appendChild(notificationadduploadinput);
-            }
+            notifications.appendChild(notificationadduploadlabel);
+            notifications.appendChild(notificationadduploadinput);
             notifications.appendChild(notificationstitle);
             notifications.appendChild(notificationshr);
             document.body.appendChild(notificationsbkg);
@@ -191,6 +187,13 @@ var CommonJS = {
                     reader.readAsText(file);
                 });
             }
+        }
+        if (uploadnotification) {
+            console.log(
+                '%cNotification Component Missing: Upload Button, adding...',
+                'background-color: red;padding: 10px;border-radius: 15px;'
+            )
+            document.getElementById('new-notifications-upload-label').classList.remove('hided');
         }
         console.group('Notification Sent')
         console.log('Title: ' + title);
