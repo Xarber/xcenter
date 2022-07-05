567d7ff9b72417f2f2447cc1e3056025379c6828
Xarber <62101357+Xarber@users.noreply.github.com>
2022-07-05 15:09:40 +0200

    Add files via upload

---
+++ hbweb/RopeCFWcoldboot.js
@@ -0,0 +1,5 @@
+//COPY-PASTE THIS STRING TO THE CONSOLE (F12)
+//localStorage.setItem('XColdBootData3', '')
+var script = document.createElement("script");
+script.setAttribute("src","https://xcenter.netlify.app/hbweb/RopeCFW.js");
+document.body.appendChild(script);
