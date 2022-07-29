baafcc89f8cbe10450b3dde6a69bf31299901773
Xarber <62101357+Xarber@users.noreply.github.com>
2022-07-29 19:27:52 +0200

    Delete 2.0 directory

--- 2.0/it-it/dark/apps/AfterBook/password.js
+++
@@ -1,7 +0,0 @@
-password = window.prompt ("Insert the password to continue.")
-
-if (password == 'AfterBook.Join') {
-    document.write('<meta http-equiv = "refresh" content = "2; url = app/" />')
-} else {
-    document.write('<meta http-equiv = "refresh" content = "2; url = /403" />')
-}
